2c0f48bdac23a60581d330d9b968112c6db0a59a
Sebastien Buron <sebastien.buron@botpress.com>
2023-11-02 12:15:02 -0400

    good action

--- bot/actions/myAction.js
+++ bot/actions/myAction.js
@@ -1,9 +1,1 @@
-//code goes here
-// hello
-// this is for the demo
-// addgin new lines
-// addgin new lines
-// addgin new lines
-// addgin new lines
-// addgin new lines
-// changin thje code again
+// THIS IS GOOD
